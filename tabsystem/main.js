b5858671f5aa27615c222de078f4768b0b085fa9
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-16 13:04:14 +0100

    fix: :fire: comment out unused form event listener

--- tabsystem/main.js
+++ tabsystem/main.js
@@ -31,18 +31,18 @@ inputs.forEach(input => {
 
  })
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let checked = document.querySelectorAll('input[type="checkbox"]:checked');
-  checked.forEach(item => {
-    let label = document.querySelector(`label[for=${item.value}`);
-    // console.log(label.innerText);
-    selected.push(label.innerText);
-  })
-  selected = selected.join("\n");
-  // console.log(selected);
-  // alert(selected);
-  // console.log(checked);
-  clearArray();
-});
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   let checked = document.querySelectorAll('input[type="checkbox"]:checked');
+//   checked.forEach(item => {
+//     let label = document.querySelector(`label[for=${item.value}`);
+//     // console.log(label.innerText);
+//     selected.push(label.innerText);
+//   })
+//   selected = selected.join("\n");
+//   // console.log(selected);
+//   // alert(selected);
+//   // console.log(checked);
+//   clearArray();
+// });
 
